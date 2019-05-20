369644f4322f2e9072d53f18f7e106428c1827b0
NathanZanelato <NathanZanelato@hotmail.com>
2019-05-20 18:25:38 -0300

    Criação da estrutura inicial do projeto

---
+++ src/main/java/org/conselhotutelar/filters/TimeZoneFilter.java
@@ -0,0 +1,34 @@
+package org.conselhotutelar.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.TimeZone;
+
+@WebFilter(filterName = "TimeZoneFilter", urlPatterns = { "/api.conselho/*" })
+public class TimeZoneFilter implements Filter {
+
+    /**
+     // Filtro para configurar o TimeZone da api.
+     // Todas as requisicoes que passarem por "/api.conselho" vao passar por doFilter()
+     */
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        // configura o timezone na JVM do Java para todas as URLs a partir de /api.conselho
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC-3"));
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig config) {
+    }
+
+}
---
+++ src/main/java/org/conselhotutelar/rest/ApplicationRoot.java
@@ -0,0 +1,8 @@
+package org.conselhotutelar.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api.conselho")
+public class ApplicationRoot extends Application {
+}
